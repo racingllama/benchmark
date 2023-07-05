78cb958f3831aef60f6da6f087c7396ff7e8b91f
Soleblaze <soleblaze@skyshadows.net>
2023-07-04 23:42:22 -0500

    Add error checking to parsing models in dir

--- rlb/llama_bench/utils.py
+++ rlb/llama_bench/utils.py
@@ -17,18 +17,22 @@ def parse_directory(directory):
 
     for file in os.listdir(directory):
         if file.endswith(".bin"):
-            tmp_name = re.split(r"\d+[bB]", file.split(".")[0])
-            name = tmp_name[0].replace("-", " ").strip()
-            if tmp_name[1] != "":
-                name += f" ({tmp_name[1].replace('-', ' ').strip()})"
-            quant = re.findall(r"q\d+[A-Za-z_0-9]*", file)[0]
-            parameters = re.findall(r"\d+[bB]", file)[0]
-            models.append(
-                {
-                    "name": name,
-                    "path": os.path.join(directory, file),
-                    "quant": quant,
-                    "parameters": parameters.upper(),
-                }
-            )
+            try:
+                tmp_name = re.split(r"\d+[bB]", file.split(".")[0])
+                name = tmp_name[0].replace("-", " ").strip()
+                if len(tmp_name) != 1 and tmp_name[1] != "":
+                    name += f" ({tmp_name[1].replace('-', ' ').strip()})"
+                quant = re.findall(r"q\d+[A-Za-z_0-9]*", file)[0]
+                parameters = re.findall(r"\d+[bB]", file)[0]
+                models.append(
+                    {
+                        "name": name,
+                        "path": os.path.join(directory, file),
+                        "quant": quant,
+                        "parameters": parameters.upper(),
+                    }
+                )
+            except IndexError:
+                print(f"Error parsing {file}")
+                continue
     return models
